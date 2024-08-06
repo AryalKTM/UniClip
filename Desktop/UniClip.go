6e8a7e4c7671a849511ace059fae0752570576f5
AryalKTM <aryalbipashree@gmail.com>
2024-08-06 22:42:56 +0545

    Variables Cleanup

--- Desktop/UniClip.go
+++ Desktop/UniClip.go
@@ -1,22 +1,8 @@
-// +build darwin
-
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"runtime"
-)
-
-var (
-	secondsBetweenChecksForClipChange = 1
-	listOfClients  = make([]*bufio.Writer, 0)
-	localClipboard string
-	printDebugInfo = false
-	cryptoStrength = 16384
-	secure         = false
-	password       []byte
-	port           = 8085
 )
 
 func main() {
